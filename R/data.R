49cb070930545b0310aa49327dd2a7eb145f31a7
tim <liztwb@users.noreply.github.com>
2022-09-22 21:44:57 +0100

    chnged from data frame to xts object

--- R/data.R
+++ R/data.R
@@ -3,12 +3,12 @@
 #' An xts object containing the daily prices of google stock
 
 #'
-#' @format A data frame with 3957 rows and 6 variables:
+#' @format An xts object with 3957 rows and 6 variables:
 #' \describe{
 #'   \item{GOOG.Open}{Open price, in US dollars}
 #'   \item{GOOG.High}{Open price, in US dollars}
-#'   \item{GOOG.Low}{Open price, in US dollars}
-#'   \item{GOOG.Close}{Open price, in US dollars}
-#'  \item{GOOG.Volume}{Open price, in US dollars}}
-#' @source quantmod run
+#'   \item{GOOG.Low}{Daily low price, in US dollars}
+#'   \item{GOOG.Close}{Daily closing price, in US dollars}
+#'  \item{GOOG.Volume}{Volume transacted, in US dollars}}
+#' @source quantmod
 "GOOG"
