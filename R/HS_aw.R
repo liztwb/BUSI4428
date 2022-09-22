c8dbf3dcb264b9edbfc599de23eb390ff42ea47e
tim <liztwb@users.noreply.github.com>
2022-09-22 21:44:21 +0100

    changed ci to cl

--- R/HS_aw.R
+++ R/HS_aw.R
@@ -3,7 +3,7 @@
 #' @param r a vector of returns
 #' @param P portfolio value
 #' @param lam lambda - the weighting parameter
-#' @param ci confidence for VaR and ES
+#' @param cl confidence level for VaR and ES
 #'
 #' @return a vector containing VaR and ES
 #' @export
@@ -11,8 +11,8 @@
 #' @examples
 #'  x <- rnorm(100)
 #'  HS_aw(x)
-HS_aw <- function(r, P = 1000, lam = 0.98, ci = 0.95){
-  alpha <- 1-ci
+HS_aw <- function(r, P = 1000, lam = 0.98, cl = 0.95){
+  alpha <- 1-cl
   n <- length(r)
   df <- data.frame(age = seq(n,1), r, loss = -P*r)
   df$w <- with(df,((1-lam)*lam^(age-1))/(1-lam^n))
@@ -23,6 +23,6 @@ HS_aw <- function(r, P = 1000, lam = 0.98, ci = 0.95){
   df2$nw <- with(df2, w/sum(w))
   ES <- with(df2, sum(nw * loss))
   res.vec <- c(VaR = VaR, ES = ES)
-  names(res.vec) <- paste0(names(res.vec),100*ci)
+  names(res.vec) <- paste0(names(res.vec),100*cl)
   return(res.vec)
 }
